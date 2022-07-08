aa15080b66bf0536971377b180ec2d367762173d
malangka0 <style246@naver.com>
2022-07-08 14:27:53 +0900

    프런트 단 javascript 파일 만들기

--- app/app.js
+++ app/app.js
@@ -10,6 +10,8 @@ const home = require("./src/routes/home");  // /routes/home 폴더에서 exports
 // 앱 세팅
 app.set("views", "./src/views");            // views 폴더의 파일을 확인
 app.set("view engine", "ejs");          // html이 아닌 ejs 파일로 읽도록 엔진 설정
+app.use(express.static(`${__dirname}/src/public`));
+
 
 app.use("/", home);                     // use -> 미들 웨어를 등록해주는 메서드
 
