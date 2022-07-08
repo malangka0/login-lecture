f127fa949d37dd54530d57ac533d2f1dadac86f1
malangka0 <style246@naver.com>
2022-07-08 16:29:06 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -13,4 +13,12 @@ function login() {
     };
 
     console.log(req);
+    console.log(JSON.stringify(req));
+    fetch("/login", {
+        method : "POST",
+        headers : {
+            "Content-Type" : "application/json",
+        },
+        body : JSON.stringify(req)
+    });
 }
