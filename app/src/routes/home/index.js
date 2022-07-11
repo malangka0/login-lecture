b1bc7db2e9839de828dfd38a3d91e02089db2175
malangka0 <style246@naver.com>
2022-07-11 13:29:16 +0900

    회원가입 페이지 구현

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -7,6 +7,7 @@ const ctrl = require("./home.ctrl");
 
 router.get("/", ctrl.output.home);                      // 요청하는 페이지로 이동 (/, /login 은 페이지 주소)
 router.get("/login", ctrl.output.login);
+router.get("/register", ctrl.output.register);
 router.post("/login", ctrl.process.login);
 
 module.exports = router;                    // 폴더 외부에서 사용할 수 있도록 설정
